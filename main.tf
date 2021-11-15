b923fde36198535af0630df49135f59d28635891
DJ Picard <dj.picard@onepeloton.com>
2021-11-15 15:05:21 -0500

    switched to new cloudfront format

--- main.tf
+++ main.tf
@@ -9,25 +9,13 @@ resource "aws_cloudfront_origin_access_identity" "cloudfront_origin_access" {}
 
 module "cloudfront_peloton" {
   source  = "spacelift.io/djpicard/cloudfront-peloton/aws"
-  version = "0.1.2"
+  version = "0.1.3"
 
   # Required inputs 
-  bucket_name = module.s3_peloton.bucket_id
+  comment        = ""
+  logging_bucket = module.s3_peloton.logging_bucket
 
   # Optional inputs 
-  # acm_certificate_arn            = string
-  aliases = [""]
-  # cache_behavior_allowed_methods = list(any)
-  # cache_behavior_cached_methods  = list(any)
-  cloudfront_default_certificate = true
-  # comment                        = string
-  default_root_object = ""
-  # enabled                        = bool
-  # is_ipv6_enabled                = bool
-  # lambda_arn                     = string
-  logging_config_bucket = module.s3_peloton.log_bucket_id
-  # logging_config_include_cookies = bool
-  origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_origin_access.id
-  # price_class                    = string
-  region = var.region
+  domain_name = "spacelift.dev-benefits.onepeloton.com"
+  region      = var.region
 }
