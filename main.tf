db032969c1b899dd18b0ad9c0d683cf1d8227066
DJ Picard <dj.picard@onepeloton.com>
2021-11-15 15:51:45 -0500

    switching to 0.1.2 cloudfront for a new poc address

--- main.tf
+++ main.tf
@@ -9,13 +9,25 @@ resource "aws_cloudfront_origin_access_identity" "cloudfront_origin_access" {}
 
 module "cloudfront_peloton" {
   source  = "spacelift.io/djpicard/cloudfront-peloton/aws"
-  version = "0.1.3"
+  version = "0.1.2"
 
   # Required inputs 
-  comment        = ""
-  logging_bucket = module.s3_peloton.log_bucket_id
+  bucket_name = module.s3_peloton.bucket_id
 
   # Optional inputs 
-  domain_name = "spacelift.dev-benefits.onepeloton.com"
-  region      = var.region
+  acm_certificate_arn = "arn:aws:acm:us-east-1:152245890419:certificate/fae33519-87e6-40b1-b3c6-177d13222af1"
+  aliases             = ["spacelift-poc.onepeloton.com"]
+  # cache_behavior_allowed_methods = list(any)
+  # cache_behavior_cached_methods  = list(any)
+  # cloudfront_default_certificate = false
+  # comment                        = string
+  default_root_object = ""
+  # enabled                        = bool
+  # is_ipv6_enabled                = bool
+  # lambda_arn                     = string
+  logging_config_bucket = module.s3_peloton.log_bucket_id
+  # logging_config_include_cookies = bool
+  origin_access_identity = aws_cloudfront_origin_access_identity.cloudfront_origin_access.id
+  # price_class                    = string
+  region = var.region
 }
